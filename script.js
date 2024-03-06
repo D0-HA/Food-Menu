12e1279d259e407d8b878421321ee0fc94195747
D0-HA <withchemi970@gmail.com>
2024-03-06 22:50:42 +0900

    Feat: Add 메뉴 버튼 및 메뉴 정보 출력 함수

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const menu = [
+const data = [
   {
     id: 1,
     title: "pancakes",
@@ -80,3 +80,76 @@ const menu = [
     desc: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil repellendus repellat exercitationem commodi! Cumque perferendis molestias iste aliquam, nulla, qui reprehenderit ea, sequi eveniet dolorem praesentium aliquid aperiam vel libero!`,
   },
 ];
+
+window.addEventListener("DOMContentLoaded", () => {
+  displayMenuButtons();
+  displayMenuItems(data);
+});
+
+const buttonArea = document.querySelector(".button-box");
+const sectionArea = document.querySelector(".section-box");
+
+function displayMenuButtons() {
+  const categories = data.reduce(
+    (acc, curr) => {
+      if (!acc.includes(curr.category)) {
+        acc.push(curr.category);
+      }
+      return acc;
+    },
+    ["all"]
+  ); // 중복되지 않은 모든 종류의 카테고리 배열을 만듦
+
+  const categoryButtons = categories
+    .map((category) => {
+      return `<button type="button" class="filter-button" data-id=${category}>
+      ${category}
+    </button>`;
+    }) // 각 카테고리를 문자열 형태로 반환
+    .join(""); // 반환된 배열 내의 문자열들을 빈 문자열로 연결
+
+  buttonArea.innerHTML = categoryButtons;
+
+  const filterButtons = buttonArea.querySelectorAll(".filter-button");
+
+  filterButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.id;
+
+      const menuCategory = data.filter((dataItem) => {
+        if (dataItem.category === category) {
+          return dataItem;
+        }
+      });
+      if (category === "all") {
+        displayMenuItems(data);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
+} // 클릭한 버튼의 id와 data.category를 비교하여 일치하면 출력
+
+function displayMenuItems(dataItems) {
+  let displayMenu = dataItems
+    .map((item) => {
+      return `<article class="item">
+    <div class="item-cover">
+    <img src=${item.img} alt=${item.title} class="item-src" />
+  </div>
+      
+      <div class="item-info">
+          <div class="item-info_header">
+              <h3>${item.title}</h3>
+              <h3 class="price">${item.price}</h3>
+          </div>
+          <p class="item-info_text">
+              ${item.desc}
+          </p>
+      </div>
+  </article>`;
+    }) // 각 아이템 정보를 문자열 형태로 반환
+    .join(""); // 반환된 배열 내의 문자열들을 빈 문자열로 연결
+
+  sectionArea.innerHTML = displayMenu;
+}
